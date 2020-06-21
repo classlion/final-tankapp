c3863324af38cf432f59aa07d64687333b8915ea
classlion <classlion1@gmail.com>
2020-06-21 23:59:24 +0300

    add slider, change hero

--- js/main.js
+++ js/main.js
@@ -1,13 +1,46 @@
 const toggle = document.getElementsByClassName("navbar-toggle");
 
 toggle[0].addEventListener("click", function(event) {
-  event.preventDefault();
-  this.classList.toggle("is-active");
+    event.preventDefault();
+    this.classList.toggle("is-active");
 });
 
 const closeNavbarMobile = document.getElementsByClassName("close");
 
 closeNavbarMobile[0].addEventListener("click", function(event) {
-  event.preventDefault();
-  toggle[0].classList.remove("is-active");
+    event.preventDefault();
+    toggle[0].classList.remove("is-active");
+});
+
+
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        800: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        },
+
+        900: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        }
+    }
 });
